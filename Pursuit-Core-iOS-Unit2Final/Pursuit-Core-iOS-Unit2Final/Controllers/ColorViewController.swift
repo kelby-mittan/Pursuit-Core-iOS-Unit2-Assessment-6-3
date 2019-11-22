d89a7134b09141ac429d3c8ed31c314f60d1350d
Kelby Mittan <kelbymittan@pursuit.org>
2019-11-22 10:09:22 -0500

    Set up my main.storyboard and two ViewControllers

---
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/ColorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ColorViewController.swift
+//  Pursuit-Core-iOS-Unit2Final
+//
+//  Created by Kelby Mittan on 11/22/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import UIKit
+
+class ColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonListViewController.swift
+++ Pursuit-Core-iOS-Unit2Final/Pursuit-Core-iOS-Unit2Final/Controllers/CrayonListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CrayonListViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
